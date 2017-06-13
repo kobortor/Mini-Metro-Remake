13c76f1f950c4a615ce1dd555ca7a69e06516088
Joey Yu <kobortor@gmail.com>
2017-06-13 11:56:58 -0400

    Added drawing documentation / rules7

--- game/segment.h
+++ game/segment.h
@@ -1,6 +1,25 @@
 #pragma once
 #include<SFML\Graphics.hpp>
 
+/*
+--------------------------------
+---RULES FOR DRAWING SEGMENTS---
+--------------------------------
+
+A station is first selected within radius R1.
+Only when we go out of this radius do we start drawing.
+
+Divide the map into 8 sections, NSEW, and the in-betweens
+When we first go out of the selection radius, find the section it is closest to.
+We can use cross product to achieve this.
+Switch the direction only when it crosses over another section.
+If N is 0 degreees, the line will only switch once it crosses 45 degrees or -45 (315) degrees. 
+       X
+      /
+     /
+Y---/
+*/
+
 //A short line, many segments joined together will form a full line
 //When a train goes through a line, it will create temporary segments, then ask the full line for the next station
 class segment : public sf::Drawable {
