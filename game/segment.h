38b51c7d18ab8d97176eff499658e98767a3b6d6
Joey Yu <kobortor@gmail.com>
2017-06-12 21:56:34 -0400

    Added segments class

---
+++ game/segment.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<SFML\Graphics.hpp>
+
+//A short line, many segments joined together will form a full line
+//When a train goes through a line, it will create temporary segments, then ask the full line for the next station
+class segment : public sf::Drawable {
+
+};
