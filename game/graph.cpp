fef19320e6898c9235789cca5313c63ef5f77648
Joey Yu <kobortor@gmail.com>
2017-06-16 13:41:32 -0400

    Added djikstras to the graph class

--- game/graph.cpp
+++ game/graph.cpp
@@ -1,10 +1,16 @@
 #include "graph.h"
 #include <algorithm>
+#include <queue>
+#include <functional>
 
 std::unordered_map<station*, std::list<station*>> graph::adj;
 
 void graph::initalize() {
 
+}
+
+void graph::add_station(station *stn) {
+	adj[stn];
 }
 
 void graph::add_link(station *first, station *second) {
@@ -14,6 +20,50 @@ void graph::add_link(station *first, station *second) {
 void graph::erase_link(station *first, station *second) {
 	adj[first].erase(std::find(adj[first].begin(), adj[first].end(), second));
 	adj[second].erase(std::find(adj[second].begin(), adj[second].end(), first));
+}
+
+int graph::shortest_dist(std::list<station*> source, std::list<station*> dest) {
+	std::unordered_map<station*, int> distances;
+
+	//may use floats later for distance
+	std::priority_queue<std::pair<int, station*>, std::vector<std::pair<int, station*>>, std::greater<std::pair<int, station*>>> pq;
+	for (station *stn : source) {
+		pq.push({ 0, stn });
+		distances[stn] = 0;
+	}
+
+	while (!pq.empty()) {
+		auto fr = pq.top();
+		auto links = adj.find(fr.second);
+
+		if (fr.first > distances[fr.second]) {
+			continue;
+		}
+
+		if (links != adj.end()) {
+			for (station *stn : links->second) {
+				auto dest = distances.find(stn);
+				if (dest == distances.end()) {
+					distances[stn] = fr.first + 1;
+					pq.push({ fr.first + 1, stn });
+				} else if(distances[stn] > fr.first + 1) {
+					distances[stn] = fr.first + 1;
+					pq.push({ fr.first + 1, stn });
+				}
+			}
+		}
+	}
+
+	int ans = -1;
+	for (station *stn : dest) {
+		auto iter = distances.find(stn);
+		if (iter != distances.end()) {
+			if (ans == -1 || iter->second < ans) {
+				ans = iter->second;
+			}
+		}
+	}
+	return ans;
 }
 
 void graph::cleanup() {
--- game/graph.h
+++ game/graph.h
@@ -9,8 +9,13 @@ class graph {
 public:
 	graph() = delete;
 	static void initalize();
+	static void add_station(station *stn);
 	static void add_link(station *first, station *second);
 	static void erase_link(station *first, station *second);
+
+	//returns the shortest distance from source to dest
+	//returns -1 if cannot reach
+	static int shortest_dist(std::list<station*> source, std::list<station*> dest);
 	static void cleanup();
 protected:
 	static std::unordered_map<station*, std::list<station*>> adj;
